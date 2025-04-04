cdf9b65241e9819a381fbe3d013caaf01b9b98c9
Ankur Seth <iankurseth@gmail.com>
2025-04-04 19:19:55 +0530

    Now --pixels can be specified

--- repub.py
+++ repub.py
@@ -168,7 +168,7 @@ def process_epub(epub_path, cmd_args):
 	fonts_should_be_removed = not cmd_args.keep_fonts
 	images_should_be_compressed = not cmd_args.keep_images
 	replace = cmd_args.replace
-	max_image_pixels = 720
+	max_image_pixels = cmd_args.pixels or 720
 	suffix = 'lean'
 
 	if not os.path.isfile(epub_path):
@@ -280,6 +280,7 @@ def main():
 	# Boolean flags with short and long options
 	parser.add_argument("--keep-fonts", action="store_true", help="Don't remove custom fonts")
 	parser.add_argument("--keep-images", action="store_true", help="Don't compress images")
+	parser.add_argument("--pixels", type=int, help="Set max pixel size for images")
 
 	parser.add_argument("-D", "--directory", action="store_true", help="Process directories as well")
 	parser.add_argument("-R", "--recursive", action="store_true", help="Process directories recursively")
